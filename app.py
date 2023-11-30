9e30fe1b144ce14c5c132c2da930aff6bb2a3dae
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-11-30 20:24:57 +0530

    Added the face and eye detection

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask
+import cv2
 
-app = Flask(__name__)
+def indentify():
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    mouth_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalcatface.xml')
+    
+    capture = cv2.VideoCapture(0)
 
-if __name__=="__main__":
-    app.run(debug=True)
+    while True:
+        ret, frame = capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+        for( x, y, w, h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w] 
+            roi_color = frame[y:y+h, x:x+w]
+
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for( ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        
+        cv2.imshow('Facial Features Extraction', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    capture.release()
+    cv2.destroyAllWindows()
+
+indentify()
