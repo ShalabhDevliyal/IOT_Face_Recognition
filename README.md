a4e10a6b7360c2a4a351e761871ae98a71809e50
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-12-01 15:31:30 +0530

    Face gets saved

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ The problem statement for the flow chart is:
 => The system should also be able to fetch an image from the database and compare it to a new image taken by the camera, and perform an operation based on the result of the comparison.
 
 => The system should use a face recognition algorithm to determine the similarity between two images and output a score or a label.
+
+What have we achieved?
+30-11-2023 => Through the pre-trained haarcascade file we are able to detect face and eyes in the face
+01-12-2023 => Through the imwrite function saving the frame with the detected face
+Task : @Get a cropped image of the face only 
--- app.py
+++ app.py
@@ -13,15 +13,17 @@ def indentify():
 
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-        for( x, y, w, h) in faces:
+        for i, ( x, y, w, h) in enumerate(faces):
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
             roi_gray = gray[y:y+h, x:x+w] 
             roi_color = frame[y:y+h, x:x+w]
 
+            cv2.imwrite(f'face_{i+1}.png',frame)
+
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for( ex, ey, ew, eh) in eyes:
                 cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-        
+
         cv2.imshow('Facial Features Extraction', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
